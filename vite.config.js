a05ba749b3b00e1fd96dbcc1c6af624022987768
putra maajid <putramaajid93@gmail.com>
2024-11-21 11:31:04 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,20 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+
+// https://vitejs.dev/config/
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
-});
+  plugins: [
+    laravel({
+      input: ['resources/js/app.js', 'resources/css/app.css'], // Sesuaikan path jika berbeda
+      refresh: true, // Mengaktifkan hot-reload untuk Laravel
+    }),
+  ],
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+    hmr: {
+      host: 'localhost',
+    },
+  },
+})
